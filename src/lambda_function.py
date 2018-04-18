46ebb17ae1aa734ece98702128bbb5c53c2e962f
Matt Simmons <bandman@gmail.com>
2018-04-18 10:43:23 -0700

    Adding some sms love

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,17 +1,27 @@
 import json
 import os
+import boto3
+
+targetArn = "arn:aws:sns:us-east-1:125387665580:matt_only_sms"
 
 print('Loading function')
-print(os.environ)
 
 def lambda_handler(event, context):
     message = str(event)
 
     body = json.loads(event['body'])
-    print(body)
-    return {
-        'statusCode': 200,
+    sns = boto3.client('sns')
+    try:
+      response = sns.publish(TopicArn = targetArn, Message=event['body'])
+      return {
+          'statusCode': 200,
+          'headers': { 'Content-Type': 'application/json' },
+          'body': event['body']
+      }
+    except Exception as err:
+      return {
+        'statusCode': 400,
         'headers': { 'Content-Type': 'application/json' },
-        'body': event['body']
-    }
+        'body': 'Failed'
+      }
 
