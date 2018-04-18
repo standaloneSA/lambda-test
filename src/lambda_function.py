f1226798fa256cf687b6fcd2545a44082cf1b2c5
Matt Simmons <bandman@gmail.com>
2018-04-18 10:57:25 -0700

    Pass through the message directly

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -7,17 +7,18 @@ targetArn = "arn:aws:sns:us-east-1:125387665580:matt_only_sms"
 print('Loading function')
 
 def lambda_handler(event, context):
-    message = str(event)
+    message = json.loads(event['body']['action'])
 
     body = json.loads(event['body'])
     sns = boto3.client('sns')
     try:
-      response = sns.publish(TopicArn = targetArn, Message=event['body'])
+      response = sns.publish(TopicArn = targetArn, Message=message)
       return {
           'statusCode': 200,
           'headers': { 'Content-Type': 'application/json' },
           'body': event['body']
       }
+
     except Exception as err:
       return {
         'statusCode': 400,
